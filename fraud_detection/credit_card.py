c3cbe4be29c9166dd258a2fb1bfed8830adeafeb
hpghsu1314 <143025880+hpghsu1314@users.noreply.github.com>
2023-12-25 05:38:31 +0000

    Fixed issue of not training

--- fraud_detection/credit_card.py
+++ fraud_detection/credit_card.py
@@ -6,7 +6,7 @@ from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
 import torch.optim as optim
 
-def remove_outliers(dataset, idx_set):
+def find_outliers(dataset, idx_set):
     """
     remove outliers from a dataset
 
@@ -17,15 +17,15 @@ def remove_outliers(dataset, idx_set):
     Returns a set of indices that represent what needs to be removed
     """
     outlier_idx = idx_set
-    # mean_val = np.mean(dataset)
-    # std_dev = np.std(dataset)
-    # lower_bound, upper_bound = mean_val - 3*std_dev, mean_val + 3*std_dev
+    mean_val = np.mean(dataset)
+    std_dev = np.std(dataset)
+    lower_bound, upper_bound = mean_val - 3*std_dev, mean_val + 3*std_dev
 
     #box and whiskers 
-    quartile25, quartile75 = np.percentile(dataset, 25), np.percentile(dataset,75)
-    interquart_range = quartile75 - quartile25
-    cutoff = interquart_range*1.75
-    lower_bound, upper_bound = quartile25 - cutoff, quartile75+cutoff
+    #quartile25, quartile75 = np.percentile(dataset, 25), np.percentile(dataset,75)
+    #interquart_range = quartile75 - quartile25
+    #cutoff = interquart_range*1.75
+    #lower_bound, upper_bound = quartile25 - cutoff, quartile75+cutoff
 
     #iterate through data set and find the outliers
     for idx in range(0,len(dataset)):
@@ -46,8 +46,6 @@ def validate(set, model):
     actual = set[:, -1].tolist()
     predicted = model(input).view(-1).tolist()
     rounded_predicted = [float(round(pred)) for pred in predicted]
-    print("actual", actual)
-    print("predicted", rounded_predicted)
 
     correct = 0
     for pred, act in zip(actual, rounded_predicted):
@@ -103,15 +101,13 @@ class FraudDetectionModel(nn.Module):
             -output_size: an int representing the number of output features
         """
         super(FraudDetectionModel,self).__init__()
-        hidden_size2 = int(hidden_size*2/3) + output_size
+        hidden_size2 = int(hidden_size*2)
         self.model = nn.Sequential(
             nn.Linear(input_size, hidden_size),
-            nn.BatchNorm1d(hidden_size),
             nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size2),
-            nn.BatchNorm1d(hidden_size2),
+            nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(hidden_size2, output_size),
+            nn.Linear(hidden_size, output_size),
             nn.Sigmoid()
         )
     
@@ -120,30 +116,39 @@ class FraudDetectionModel(nn.Module):
         return self.model(x)
 
 
+
 # reading/extracting data from the file
-df = pd.read_csv('data/creditcard.csv')
+df = pd.read_csv('creditcard.csv')
+information = df.values.tolist()
 
 # find all the outliers
 headers = list(df.columns)
-outliers = set()
-for idx in range(1,len(headers)-2):
-    header = headers[idx]
-    outliers = remove_outliers(df[header], outliers)
-
-# remove the rows that include the outliers
-filtered = df.values.tolist()
-idx_outliers = sorted(outliers, reverse=True)
-for idx in idx_outliers:
-    filtered.pop(idx)
+not_fraud_outliers = set()
+fraud_outliers = set()
 
 # separate the fraud and non fraud data
 fraud = []
 not_fraud = []
-for idx in range(0, len(filtered)):
-    if filtered[idx][-1] == 0:
-        not_fraud.append(filtered[idx])
+for idx in range(0, len(df)):
+    if df["Class"][idx] == 0:
+        not_fraud.append(information[idx])
     else:
-        fraud.append(filtered[idx])
+        fraud.append(information[idx])
+
+for idx in range(1,len(headers)-2):
+    not_fraud_data = [not_fraud[i][idx] for i in range(len(not_fraud))]
+    fraud_data = [fraud[i][idx] for i in range(len(fraud))]
+    not_fraud_outliers = find_outliers(not_fraud_data, not_fraud_outliers)
+    fraud_outliers = find_outliers(fraud_data, fraud_outliers)
+
+not_fraud_outlier_idx = sorted(not_fraud_outliers, reverse=True)
+fraud_outlier_idx = sorted(fraud_outliers, reverse=True)
+
+for idx in not_fraud_outlier_idx:
+    not_fraud.pop(idx)
+
+for idx in fraud_outlier_idx:
+    fraud.pop(idx)
 
 fraud_df = pd.DataFrame(fraud, columns=headers)
 not_fraud_df = pd.DataFrame(not_fraud, columns=headers)
@@ -160,33 +165,32 @@ hidden_size = int(input_size*3/4) + output_size
 model = FraudDetectionModel(input_size, hidden_size, output_size)
 
 # optimizer and loss function
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
 loss_func = nn.BCELoss()
 
 # creating the validation set
 # validation_set = torch.tensor(fraud[0:401] + not_fraud[0:401])
-validation_set = torch.tensor(fraud[0:401] + not_fraud[0:401], dtype=torch.float32)
-print(validation_set.shape)
+validation_set = torch.tensor(fraud[int(len(fraud)*0.75):] + not_fraud[int(len(not_fraud)*0.75):int(len(not_fraud)*0.75)+100])
 
 # training loop
-epoch_num = 20
-model.train()
+epoch_num = 100
+print("Finished Setup")
+
 for epoch in range(0, epoch_num):
+    model.eval()
+    print(validate(validation_set, model))
+    model.train()
     for transaction in train_dataloader:
         input = transaction[:, :-1]
         actual = transaction[:,-1]
-
-        # optimize and backpropogate
-        optimizer.zero_grad()
         output = model(input)
         loss = loss_func(output, torch.reshape(actual,(64,1)))
+        
+        # optimize and backpropogate
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-
-    # #validate the data
-    # if (epoch + 1)%10 == 0:
-    #     print(validate(validation_set, model))
-
+    
 
 test_set = torch.tensor(fraud[int(len(fraud)*0.75):] + not_fraud[int(len(not_fraud)*0.75):])
 print(validate(test_set, model))
