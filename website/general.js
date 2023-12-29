b1123da1ec34675c5a71a5c6b6d80904ec4ea1f5
gy180 <142842008+gy180@users.noreply.github.com>
2023-12-29 02:52:28 +0000

    search bar

--- website/general.js
+++ website/general.js
@@ -3,3 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     document.body.style.opacity = 1; // Set opacity to 1 to trigger the fade-in animation
     document.querySelector('header').style.opacity = 1; // Set opacity for the header
 });
+
+document.getElementById('navigation').style.display = 'blck';
+document.getElementById('searchBar').style.display = 'none';
+
+// search bar and navigation swap
+document.getElementById('searchButton').addEventListener('click', function() {
+    document.getElementById('navigation').style.display = 'none';
+    document.getElementById('searchBar').style.display = 'block';
+});
+
+document.getElementById('xOut').addEventListener('click', function() {
+    document.getElementById('navigation').style.display = 'block';
+    document.getElementById('searchBar').style.display = 'none';
+});
