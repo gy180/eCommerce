d18eb50e29d9a53ce1fb2fe70ec94fb7e67c63e3
Grace Yang <gy18@rice.edu>
2024-01-03 14:26:07 +0800

    header and footer

---
+++ website/header_footer/footer.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hami Tree • About</title>
+        <link rel = "icon" href = "../images/logo2.png" >
+        <link rel="stylesheet" href="../style.css" type="text/css" media="only screen">
+        <link rel="stylesheet" href="about.css" type="text/css" media="only screen">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+
+        <style type="text/css"></style>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@200;300;400;500;600;700&family=Racing+Sans+One&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <footer class="subscribe">
+            <img src="../images/subscribe background.png" class="subscribe-image">
+            <div class="toner"></div>
+            <div class="container">
+                <h1>SUBSCRIBE TO OUR EMAIL</h1>
+                <h6>Join our email list for exclusive offers and the latest news</h6>
+
+                    <div class="sub-email">
+                        <form id="subscribe-form">
+                            <input id="emailInput" type="text" name="sub-email" required="" autocomplete="off" placeholder="Email">
+                            <button type="submit" id="enterEmail">
+                                <img src="../images/arrow_forward.png">
+                            </button>
+                        </form>
+                    </div>
+
+                <div class="policies">
+                    <a href="../other_pages/tos.html">
+                        <p>Terms of Service</p>
+                    </a>
+                    <a href="../other_pages/refunds.html">
+                        <p>Refund/Cancellation</p>
+                    </a>
+                    <a href="../contact_us/contact.html">
+                        <p>Contact</p>
+                    </a>
+                    <a href="../other_pages/FAQ.html">
+                        <p>FAQ</p>
+                    </a>
+                </div>
+                <div class="socials-icons">
+                    <a href="https://www.instagram.com/hami._.tree/" target="_blank">
+                        <img src="../images/Non-BlurIG.png">
+                    </a>
+                    <a href="https://hamitree.carrd.co/" target="_blank">
+                        <img src="../images/Non-BlurLink.png">
+                    </a>
+                    <a href="hamitreestudio@gmail.com">
+                        <img src="../images/Non-BlurEmail.png">
+                    </a>
+                </div>
+            </div>
+            <div class="footer-line"></div>
+            <div class="footer-name">
+                <p>© 2024,  Hami Tree</p>
+            </div>
+        </footer>
+        
+        <script src="../general.js"></script>
+    </body>
+</html>
